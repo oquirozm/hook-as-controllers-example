c5e806bef15fbe90fac249c81865464a61b2b9b9
Omar Quiroz <omar@pragmateam.dev>
2019-11-10 00:03:39 +0100

    Testing env variables deployment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CharacterForm from './features/CharacterForm';
 
 function App() {
+  console.log(process.env);
   return (
     <div className='App'>
       <header className='App-header'>
